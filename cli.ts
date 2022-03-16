a2ca998a7d8f287a1861df0c36c6bb84a8f4ca3f
JesseTheRobot <jesse.cruz.wright@gmail.com>
2022-03-16 10:09:49 +0000

    Add CLI

---
+++ cli.ts
@@ -0,0 +1,25 @@
+import { Command } from "commander";
+import mirror from "./functions/mirror"
+import upload from "./functions/export"
+const program = new Command()
+
+// program
+//     .option("-h --host <string>", "host/local PreWeave node to communicate with")
+// .option("-t --target <string>", "host to target")
+
+program.command("mirror").description("mirrors another PreWeave node").argument("<target>", "target to mirror from")
+    .action(async (target: string) => {
+        console.log(`mirroring from ${target}`)
+        await mirror(target)
+    })
+
+
+program.command("upload").description("Uploads all exportable transactions to Bundlr")
+    .action(async () => {
+        console.log("Uploading to Bundlr")
+        await upload()
+    })
+
+// const options = program.opts();
+const argv = process.argv;
+program.parse(argv);
