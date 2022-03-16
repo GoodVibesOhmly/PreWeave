4f65df80e0905803c8828c4524ed67d040cbca19
Josh Benaron <joshbenaron@gmail.com>
2022-03-16 10:17:41 +0000

    Chore: improve routes

--- src/routes/getExportable.ts
+++ src/routes/getExportable.ts
@@ -1,14 +1,11 @@
-import { Context } from "koa";
-import { NextFunction } from "../types";
-import { httpServerConnection } from "../utils/db";
+import {Context} from "koa";
+import {NextFunction} from "../types";
+import {httpServerConnection} from "../utils/db";
 
 export async function getExportable(ctx: Context, next: NextFunction) {
-    // get all exportable items
-    const res = await httpServerConnection("transactions")
+    ctx.body = await httpServerConnection("transactions")
         .select("tx_id")
         .where("exportable", "=", true)
-
-    ctx.body = res;
-    await next()
-
+        .then(r => r.map(row => row.tx_id));
+    await next();
 }
--- src/routes/getItem.ts
+++ src/routes/getItem.ts
@@ -1,12 +1,11 @@
-import { NextFunction } from "connect";
-import { createReadStream, promises } from "fs";
-import { Context } from "koa";
-import { resolve } from "path";
-import { exists, httpServerConnection, isExportable } from "../utils/db";
-import { checkPath, isArweaveAddress, makeError } from "../utils/utils";
+import {NextFunction} from "connect";
+import {createReadStream, promises} from "fs";
+import {Context} from "koa";
+import {resolve} from "path";
+import {exists, httpServerConnection, isExportable} from "../utils/db";
+import {checkPath, isArweaveAddress, makeError} from "../utils/utils";
 
 export async function getItem(ctx: Context, next: NextFunction) {
-
     const txId = ctx.params.txId;
     if (!txId) {
         return makeError(ctx, 400, "No TxID specified")
@@ -27,13 +26,7 @@ export async function getItem(ctx: Context, next: NextFunction) {
         return makeError(ctx, 404)
     }
 
-    // const tx = await getDataItem(httpServerConnection, txId)
-    //  const di = await new FileDataItem(path).tags()
-    // todo: remove from DB and add to obj store
-
-    // ctx.setHeader("Content-Type", di.find(t => t.name.toLowerCase() === "content-type") ?? "application/octet-stream")
-    const size = (await promises.stat(path)).size
-    ctx.setHeader("Content-Length", size)
-    ctx.body = createReadStream(resolve(path))
+    ctx.length = await promises.stat(path).then(r => r.size);
+    ctx.body = createReadStream(resolve(path));
     await next();
 }
